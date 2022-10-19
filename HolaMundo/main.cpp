6e458d87fe15b75ec195bff2addaf4e3a9ee754e
Alejandro Ravera <killmess@gmail.com>
2022-10-19 16:14:04 -0300

    Arreglé el dos

--- HolaMundo/main.cpp
+++ HolaMundo/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     cout << "Hello world!" << endl;
     cout << "Uno" << endl;
-    cout << "Uno" << endl;
+    cout << "Dos" << endl;
     cout << "Tres" << endl;
     cout << "Cuatro" << endl;
     cout << "Cinco" << endl;
